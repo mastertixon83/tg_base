92faf08ea433daec0f3b08ee4c0c8b3c6118fa0f
Andrey Tikhonov <mastertixon.is@gmail.com>
2024-11-14 00:15:26 +0500

    init1

--- telegram/main/urls.py
+++ telegram/main/urls.py
@@ -8,6 +8,6 @@ app_name = 'Telega_main'
 urlpatterns = [
     path('', home_view, name='home'), # Главная страница
     path('tg_parser', run_parser_tg_view, name='tg_parser'), # Главная страница
-    path('resave', resave, name='resave'), # Главная страница
+    # path('resave', resave, name='resave'), # Главная страница
     path('channel/<int:pk>/', get_channel_prompt, name='get_channel_prompt'),
 ]
